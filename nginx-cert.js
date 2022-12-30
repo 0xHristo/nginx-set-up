c96ffa497443d529e1a3bf86d3ad33e9ad4a5150
0xHristo <hristoht.todorov@gmail.com>
2022-12-30 18:46:08 +0200

    Initial script

---
+++ nginx-cert.js
@@ -0,0 +1,33 @@
+const { execSync } = require("child_process")
+const { writeFileSync, readFileSync } = require("fs")
+
+const sitesAvailableDIR = "/etc/nginx/sites-available"
+const sitesEnabledDIR = "/etc/nginx/sites-enabled"
+
+const nginxCert = () => {
+    try {
+        const args = process.argv.slice(2)
+        const domain = args[0]
+
+        const defaultServerConfig = readFileSync("./default-server-config", "utf-8")
+
+        const domainDefaultServerConfig = defaultServerConfig.replaceAll(/<domain>/g, domain)
+
+        if(!existsSync(sitesAvailableDIR)) {
+            throw new Error(`Forlder ${sitesAvailableDIR} does not exist`)
+        }
+
+        if(!existsSync(sitesEnabledDIR)) {
+            throw new Error(`Forlder ${sitesEnabledDIR} does not exist`)
+        }
+
+        writeFileSync(`${sitesAvailableDIR}/${domain}`, domainDefaultServerConfig, "utf-8")
+        execSync(`ln -s ${sitesAvailableDIR}/${domain} ${sitesEnabledDIR}`)
+        execSync(`ufw allow 'Nginx HTTP'`)
+        execSync(`certbot certonly --standalone -d ${domain}`)
+    } catch (e) {
+        console.log(e.stack)
+    }
+}
+
+nginxCert()
